bda678867586a7d2eb34f11fd590f997bcef89c1
Filippo-Martinoli <filippomartinoli07@gmail.com>
2026-02-09 22:06:23 +0100

    Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,25 +1,60 @@
 package org.example;
-import java.io.FileNotFoundException;
+import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws FileNotFoundException {
-        Brano b1 = new Brano ("FMLEM234", "Lo So Che", "3:01", "15/11/2024 ", 44000000, 44000,"Capo Plaza");
-        String file = "Musica.json";
-        Scrittore s = new Scrittore(file);
-        s.setBrano(b1);
-        Lettore l = new Lettore(file);
-        Thread ts = new Thread(s);
-        Thread tl = new Thread(l);
-        ts.start();
-
-        Brano b2 = l.leggiDalFile();
-        System.out.println(b2);
+    public static void main(String[] args) {
+        System.out.println("APP MUSICA\n");
+        // Countdown
+        Thread c = new Thread(new Countdown());
+        c.start();
+        
         try {
-            ts.join();
+            c.join();
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-        tl.start();
+
+        Scanner sc = new Scanner(System.in);
+        List<Brano> lista = new ArrayList<>();
+        System.out.print("Quanti brani vuoi inserire? ");
+        int n = sc.nextInt();
+        sc.nextLine();
+        for (int i = 0; i < n; i++) {
+            System.out.println("\nBrano " + (i + 1));
+            System.out.print("ID: ");
+            String id = sc.nextLine();
+            System.out.print("Titolo: ");
+            String titolo = sc.nextLine();
+            System.out.print("Cantante: ");
+            String cantante = sc.nextLine();
+            System.out.print("Durata: ");
+            String durata = sc.nextLine();
+            System.out.print("Anno: ");
+            int anno = sc.nextInt();
+            System.out.print("Ascolti: ");
+            long ascolti = sc.nextLong();
+            System.out.print("Like: ");
+            int like = sc.nextInt();
+            sc.nextLine();
+            
+            lista.add(new Brano(
+                    id, titolo, cantante,
+                    durata, anno, ascolti, like
+            ));
+        }
+        
+        String file = "Musica.json";
+        GestioneFile gf = new GestioneFile();
+        Thread scrittore = new Thread(new Scrittore(file, lista, gf));
+        Thread lettore = new Thread(new Lettore(file, gf));
+        scrittore.start();
+
+        try {
+            scrittore.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        lettore.start();
     }
 }
-
