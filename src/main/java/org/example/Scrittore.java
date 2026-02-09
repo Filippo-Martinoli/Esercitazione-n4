b98c337d94ff89dd176b7fbd18a81ff73b7a8826
Filippo-Martinoli <filippomartinoli07@gmail.com>
2026-02-09 22:01:34 +0100

    Scrittore.java

--- src/main/java/org/example/Scrittore.java
+++ src/main/java/org/example/Scrittore.java
@@ -1,35 +1,22 @@
 package org.example;
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-import java.io.BufferedWriter;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.util.List;
 
 public class Scrittore implements Runnable {
-    private String musica;
-    private Brano brano;
-
-    public Scrittore(String file) {
-        this.musica = file;
+    private String file;
+    private List<Brano> lista;
+    private GestioneFile gestioneFile;
+    
+    public Scrittore(String file,
+                     List<Brano> lista,
+                     GestioneFile gestioneFile) {
+        this.file = file;
+        this.lista = lista;
+        this.gestioneFile = gestioneFile;
     }
-
-    public void setBrano(Brano brano) {
-        this.brano = brano;
-    }
-
     @Override
+    
     public void run() {
-        scriviJson();
-    }
-
-    public void scriviJson() {
-        Gson gson = new GsonBuilder().setPrettyPrinting().create();
-        String json = gson.toJson(brano);
-
-        try (BufferedWriter writer = new BufferedWriter(new FileWriter(musica))) {
-            writer.write(json);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        gestioneFile.scriviJson(file, lista);
+        System.out.println("File scritto correttamente!");
     }
 }
